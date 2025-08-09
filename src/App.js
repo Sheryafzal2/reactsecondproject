8dd376c0254803612ae6910a83076225e9986011
sheheryar afzal <sheryafzal21@gmail.com>
2025-08-10 03:36:14 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name ="why brother";
+  let 1=[20,30,40,50]
+  return (
+    <div classname="App">
+      <h1>{name}</h1>
+      { 1.map((v)=>{
+        return(
+          <div>{v}</div>
+      )
+      })}
+    </div>
+  );
+}
+
+export default App;
